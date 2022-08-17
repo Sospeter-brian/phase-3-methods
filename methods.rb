ebbda95dd7e42a90a6af47e2bb007026bcde3031
sospeterbrian029@gmail.com <sospeterbrian029@gmail.com>
2022-08-17 23:53:30 +0300

    Ruby methods done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "Sunny")
+    puts "Hello, programmer!"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+greet_with_default "Sunny"
+
+def add(num1, num2)
+    return num1 + num2
+end
+sum = add(1, 2)
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+   return num / 2
+end
+result = halve(4)
